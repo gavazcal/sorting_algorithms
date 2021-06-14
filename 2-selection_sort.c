e5f88df303c76a3792accead29b54b9bbef7a08a
Thegermanblob <thegermanblob@gmail.com>
2021-06-14 19:25:22 +0000

    Make betty compliant

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,16 +1,17 @@
 #include "sort.h"
 /**
- *
- *
+ * selection_sort - sorts an array using selection_sort algorithim
+ * @array: array to be sorted
+ * @size: size of array
  */
 void selection_sort(int *array, size_t size)
 {
 	int temp, i, i2, tracker;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		temp = array[i];
-		for(i2 = i + 1; i2 < size; i2++)
+		for (i2 = i + 1; i2 < size; i2++)
 		{
 			if (temp > array[i2])
 			{
@@ -18,7 +19,7 @@ void selection_sort(int *array, size_t size)
 				tracker = i2;
 			}
 		}
-		if(array[i] != temp)
+		if (array[i] != temp)
 		{
 			array[tracker] = array[i];
 			array[i] = temp;
