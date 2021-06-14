8241f1e5132fa03096f341b2dc69c69cb57c8817
Thegermanblob <thegermanblob@gmail.com>
2021-06-14 19:23:28 +0000

    Add task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ *
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp, i, i2, tracker;
+
+	for(i = 0; i < size; i++)
+	{
+		temp = array[i];
+		for(i2 = i + 1; i2 < size; i2++)
+		{
+			if (temp > array[i2])
+			{
+				temp = array[i2];
+				tracker = i2;
+			}
+		}
+		if(array[i] != temp)
+		{
+			array[tracker] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
+
