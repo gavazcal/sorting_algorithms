4aa413dfcf527dfa8fa08144660fb4a4ffba19de
Thegermanblob <thegermanblob@gmail.com>
2021-06-15 17:29:30 +0000

    Add functions

--- sort.h
+++ sort.h
@@ -27,4 +27,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void sorter(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
+void swap(int *array, int i, int j, size_t size);
+
 #endif
